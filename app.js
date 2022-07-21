7038ec27b3b2c480eb509996fe3d915746a2d8cc
Dharak-Koradia <61179030+Dharak-Koradia@users.noreply.github.com>
2022-07-20 17:55:03 -0700

    anyone can see the secrets. People need to sign in to submit a secret

--- app.js
+++ app.js
@@ -39,7 +39,8 @@ mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 const userSchema = new mongoose.Schema({
     email: String,
     password: String,
-    googleId: String
+    googleId: String,
+    secret: String
 });
 
 // hash and salt passwords, and save the user in DB
@@ -110,15 +111,51 @@ app.get("/register", function(req, res){
 });
 
 app.get("/secrets", function(req, res){
+    User.find({"secret": {$ne: null}}, function(err, foundUsers){
+        if (err) {
+            console.log(err);
+        }
+        else {
+            if (foundUsers) {
+                res.render("secrets", {usersWithSecrets: foundUsers});
+            }
+        }
+    });
+});
+
+app.get("/submit", function(req, res){
     if (req.isAuthenticated()) {
         // this means that they're logged in
-        res.render("secrets");
+        res.render("submit");
     }
     else {
         res.redirect("/login");
     }
-})
 
+});
+
+
+app.post("/submit", function(req, res){
+    const submittedSecret = req.body.secret;
+
+    console.log(req.user.id);
+
+    User.findById(req.user.id,  function(err, foundUser){
+        if (err) {
+            console.log(err);
+        }
+        else {
+            if (foundUser) {
+                foundUser.secret = submittedSecret;
+                foundUser.save(function() {
+                    res.redirect("/secrets");
+                });
+            }
+        }
+
+    });
+
+});
 
 app.get("/logout", function(req, res){
     req.logout(function(err){
